7a01f3519f2bb0430390f48ba9cb00ef7aa9470c
Alisher Khamidov <a.a.khamidov@yandex.ru>
2024-01-09 19:31:27 +0100

    init

---
+++ consultations/2023-11-29/code/App.tsx
@@ -0,0 +1,21 @@
+// Lessons
+// import Lesson20 from "./lessons/Lesson20";
+
+//Consultations
+import Counsultation20 from "./consultations/Consultation20";
+
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      {/* Lessons */}
+      {/* <Lesson20 /> */}
+
+      {/* Consultations */}
+      <Counsultation20 />
+    </div>
+  );
+}
+
+export default App;
